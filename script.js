f17f3fadef83be8690c325c2e1a08b9a0e49efe3
xViolaine <linda.guester@yahoo.de>
2022-05-23 15:58:53 +0100

    stuff changed

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-
 const correctAnswer = document.getElementById("correct_answer!")
-const message = document.getElementsByClassName('answer')
+const startButton = document.getElementById('start')
+const question = document.getElementById('question')
+const answers = document.getElementById('answers')
 
 
-function printMessage(){message.innerText = "You're correct"}
+function printMessage() {
+  console.log(message)
+  message.innerText = "You're correct"
+}
 
+function printQuestion1() {
+  question.innerText = "What is the best animal?"
+  const btn1 = document.createElement("button")
+  btn1.innerText = "RED PANDA!"
+  const btn2 = document.createElement("button")
+  btn2.innerText = "Ring-tailed Lemur"
+  const btn3 = document.createElement("button")
+  btn3.innerText = "Otto"
+  const btn4 = document.createElement("button")
+  btn4.innerText = "Dogs"
+}
 
+startButton.addEventListener('click', printQuestion1)
 
-correctAnswer.addEventListener('click',()=>{
-  console.log('here')
-  printMessage();
-  })
+correctAnswer.addEventListener('click', printMessage)
 
 
 
@@ -29,3 +42,18 @@ correctAnswer.addEventListener('click',()=>{
 
 // correctAnswer.addEventListener('click',()=>{count})
 
+{/* <h2>1. What is the best animal?</h2>
+<button id="correct_answer!">RED PANDA!</button>
+<button>Otter</button>
+<button>Moose</button>
+<button>Dog</button>
+<h2>2. What is the best colour?</h2>
+<button id="correct_answer!">BLUE!</button>
+<button>Red</button>
+<button>Violet</button>
+<button>Aquamarine</button>
+<h2>3. What is the best food?</h2>
+<button id="correct_answer!">SPAGHETTI CARBONARA!</button>
+<button>Scotch Eggs</button>
+<button>Tikka Masala</button>
+<button>Fajitas</button> */}
